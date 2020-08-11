2d88dbd38aa88335b30525230048590769a227e0
Raphael Cerqueira <rcerqueira@vr.com.br>
2020-08-11 13:07:19 -0300

    Initial Commit

---
+++ LeCheese/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LeCheese
+//
+//  Created by Raphael Cerqueira on 11/08/20.
+//  Copyright © 2020 Raphael Cerqueira. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
